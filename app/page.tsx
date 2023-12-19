d6c82ad009bfb6cb7d4b36a9fbe7e9b1f1771014
ssyuk <sihu@syuk.me>
2023-12-19 12:12:40 +0900

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import Link from "next/link";
+import {NavigationMenu, NavigationMenuLink, NavigationMenuList} from "@/components/ui/navigation-menu";
+import {Button} from "@/components/ui/button";
+import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import {Landing} from "@/components/component/landing";
+
+export default function Home() {
+    return (
+        <div key="1" className="bg-gray-900 text-white">
+            <Landing/>
+        </div>
+    )
+}
